fae1b949c4dad789404170c27fa345bb85f3ae86
unknown <hzyejinfu@HIH-D-8268.hz.ntes.domain>
2017-03-30 15:37:16 +0800

    no message

--- src/main/java/com/netease/okr/redis/RedisConstant.java
+++ src/main/java/com/netease/okr/redis/RedisConstant.java
@@ -1,5 +1,7 @@
 package com.netease.okr.redis;
 
+import com.netease.okr.util.PropertiesUtils;
+
 /**
  * purpose: redis key常量定义类
  * 
@@ -7,19 +9,21 @@ package com.netease.okr.redis;
  */
 public interface RedisConstant {
 	
-	String HAS_NEW_WEEK_REPORT_KEY = "OKR_HAS_NEW_WEEK_REPORT_KEY_";//是否有的周报OKR_HAS_NEW_WEEK_REPORT_+{userId}
+	String REDIS_PRE_KEY = new PropertiesUtils("config", null).getPropery("redis_pre_key");
+	
+	String HAS_NEW_WEEK_REPORT_KEY = REDIS_PRE_KEY+"OKR_HAS_NEW_WEEK_REPORT_KEY_";//是否有的周报OKR_HAS_NEW_WEEK_REPORT_+{userId}
 	
 	Integer HAS_NEW_WEEK_REPORT_EXPIRE = 84 * 60 * 60;//最新周报缓存时间【84小时】
 	
-	String WEEK_REPORT_COUNT_KEY = "OKR_WEEK_REPORT_COUNT_KEY_";//周报数量OKR_WEEK_REPORT_COUNT_+{userId}
+	String WEEK_REPORT_COUNT_KEY = REDIS_PRE_KEY+"OKR_WEEK_REPORT_COUNT_KEY_";//周报数量OKR_WEEK_REPORT_COUNT_+{userId}
 	
 	Integer WEEK_REPORT_COUNT_EXPIRE = 2 * 60 * 60;//周报数量缓存时间【2小时】
 	
-	String OBJECTIVES_COUNT_KEY = "OKR_OBJECTIVES_COUNT_KEY_";//目标数量OKR_OBJECTIVES_COUNT_+{userId}
+	String OBJECTIVES_COUNT_KEY = REDIS_PRE_KEY+"OKR_OBJECTIVES_COUNT_KEY_";//目标数量OKR_OBJECTIVES_COUNT_+{userId}
 	
 	Integer OBJECTIVES_COUNT_EXPIRE = 2 * 60 * 60;//目标数量缓存时间【2小时】
 	
-	String DATE_INFO_QUERY_KEY = "OKR_DATE_INFO_QUERY_KEY";//周报日期信息
+	String DATE_INFO_QUERY_KEY = REDIS_PRE_KEY+"OKR_DATE_INFO_QUERY_KEY";//周报日期信息
 	
 	Integer DATE_INFO_QUERY_KEY_EXPIRE = 6 * 60 * 60;//周报日期信息缓存时间【6小时】
 
